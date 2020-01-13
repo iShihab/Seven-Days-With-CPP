c41b104812d895ae88f8fd6f626815ff47c10265
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-14 03:47:04 +0600

    Input output in cpp

---
+++ InputOutput.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int input;
+    cout<<"Enter an input : ";
+    cin>>input;
+    cout<<"You have entered "<<input<<endl;
+    return 0;
+}
