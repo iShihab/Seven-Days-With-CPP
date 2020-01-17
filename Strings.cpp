1bc7a1ce92f7f0c551d609ad2347075a72d69727
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-18 01:15:06 +0600

    String Conatenation

---
+++ Strings.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string myString1= "Sajedur";
+    string myString2= "Rahman";
+    cout<<myString1+myString2<<endl;
+    return 0;
+}
+
