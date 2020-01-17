91b8ab3e0876c442ff2ff34b06e1dfea9b057201
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-18 01:16:27 +0600

    String length

--- Strings.cpp
+++ Strings.cpp
@@ -7,6 +7,12 @@ int main(int argc, char const *argv[])
     string myString1= "Sajedur";
     string myString2= "Rahman";
     cout<<myString1+myString2<<endl;
+
+    // String length
+    cout<<myString1.length();
+
     return 0;
+
+
 }
 
