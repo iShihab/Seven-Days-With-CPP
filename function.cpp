794622d8d534fe44220c393fb643af23960b00d5
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-18 02:36:00 +0600

    updatd

--- function.cpp
+++ function.cpp
@@ -1,14 +1,21 @@
 #include<iostream>
 
 using namespace std;
-
+// Function declaration
+void myFunction();
 void printThis();
 int main(int argc, char const *argv[])
 {
     printThis();
+    myFunction();
     return 0;
 }
 
 void printThis(){
     cout<<"I am called to print ;) "<<endl;
+}
+
+// Function definition
+void myFunction() {
+  cout << "I just got executed!";
 }
