f7e6ef80e63eaaed0a7b7b5ea8f3fa7efd57e9fd
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-17 19:08:49 +0600

    For loop
    inserting element in the array using for loop

---
+++ forLoop.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    int arr[5];
+    for (int i = 0; i < 5; i++)
+    {
+        
+        cin>>arr[i];
+       
+    }
+
+    for (int i = 0; i <5 ; i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+    
+    
+    return 0;
+}
