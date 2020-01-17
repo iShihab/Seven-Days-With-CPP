d6bd6dd6b56f846f41163222804e5cd67d8a276f
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-18 02:47:39 +0600

    Function with parameter

---
+++ funtionP.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+void myFunction(string name)
+{
+    cout<<"Your name is : "<<name<<endl;
+}
+
+int main(int argc, char const *argv[])
+{
+
+    myFunction("Shihab");
+    myFunction("Sajed");
+    return 0;
+}
+
