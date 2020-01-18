ed5ddbf3f8726eef705cbdbe465a42ab4e28084a
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-18 22:33:57 +0600

    Another funtion added

--- funtionP.cpp
+++ funtionP.cpp
@@ -1,25 +1,36 @@
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <string.h>
 
 using namespace std;
 void myFunction(string name)
 {
-    cout<<"Your name is : "<<name<<endl;
+    cout << "Your name is : " << name << endl;
 }
 
 //default parameter
-void func(int age=23)
+void func(int age = 23)
 {
-    cout<<age<<endl;
+    cout << age << endl;
+}
+
+int sub(int x, int y)
+{
+    int res = x - y;
+    return res;
 }
 
 int main(int argc, char const *argv[])
 {
 
-    myFunction("Shihab");
-    myFunction("Sajed");
-    func();
-    func(25);
+    // myFunction("Shihab");
+    // myFunction("Sajed");
+    // func();
+    // func(25);
+    int a = 10, b = 2;
+    int z = sub(7, 4);
+    cout << "The first result is : " << z << endl;
+    cout << "The second result is : " << sub(a, b) << endl;
+    cout << "The third result is : " << sub(30, 22) << endl;
+
     return 0;
 }
-
