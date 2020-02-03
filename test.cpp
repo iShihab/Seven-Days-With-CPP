3ad34165323751a0166e8d0e5752ff15d3de92a5
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-02-03 07:02:32 +0600

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,14 @@
+// function example
+#include <iostream>
+using namespace std;
+
+int add(int x, int y)
+{
+    int r = x + y;
+    return (r);
+}
+
+int main(int argc, char const *argv[])
+{
+    cout << "Result : " << add(2, 3);
+}
