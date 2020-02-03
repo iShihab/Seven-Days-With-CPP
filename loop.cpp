3612b43cce6c6738e31b64dac41a87196c4051f6
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-02-03 07:02:36 +0600

    Update loop.cpp

--- loop.cpp
+++ loop.cpp
@@ -1,13 +1,14 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int i=1;
-    do{
-        cout<<i<<endl;
+    int i = 1;
+    do
+    {
+        cout << i << endl;
         i++;
-    }while(i<6);
+    } while (i < 6);
     return 0;
 }
